4814d9f5653843dcc7c249f0071953fe3da69930
Wandahoi (Vini) <rmvinicius1@gmail.com>
2026-09-11 07:59:19 -0300

    removi a função de fala

--- Trabalho Quimica/script.js
+++ Trabalho Quimica/script.js
@@ -1,9 +1,1 @@
-function lerTexto() {
-    const texto = document.getElementById("texto").innerText;
-    const fala = new SpeechSynthesisUtterance(texto);
-
-    fala.lang = "pt-BR";
-    speechSynthesis.speak(fala);
-    
-
-}
+oi
